8bdaadefa20e9de9c118808c687902ab0ab5d71b
nichels02 <nicolas.derivero@tecsup.edu.pe>
2024-04-24 17:55:59 -0500

    nodos avance

---
+++ Assets/Resource/scrip/ia/Nodo/ActionNodeAction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using BehaviorDesigner.Runtime.Tasks;
+
+[TaskCategory("IA SC/Node Base")]
+public class ActionNodeAction : ActionNode
+{
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+}
